2059ff74bec0c9e2d637be947f26c0403cf3d3a0
James Todd <james9005@hotmail.co.uk>
2017-07-07 21:13:46 +0100

    added mongo and character stuff

--- SPOTCMD/Program.cs
+++ SPOTCMD/Program.cs
@@ -8,6 +8,11 @@ using SpotifyAPI.Local;
 using SpotifyAPI.Local.Models;
 using System.Threading;
 using System.Globalization;
+using MongoDB;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver;
+using System.Security.Cryptography;
+using MongoDB.Bson;
 
 namespace SPOTCMD {
   class Program {
@@ -29,60 +34,64 @@ namespace SPOTCMD {
 
         //TODO: load what is currently playing..
         StatusResponse status = _spotify.GetStatus();
+
         DisplaySong(status.Track);
         _currentTrack = status.Track;
 
         if (status.Playing || !(status.Playing)) {
           int x = 0;
           while (x == 0) {
-            String str = Console.ReadLine();
+            char str = Console.ReadKey().KeyChar;
             #region switches for input
             switch (str) {
-              case "s":
-                _spotify.Skip();
-                Thread.Sleep(100);
-                break;
-              case "p":
+              case 's':
+              _spotify.Skip();
+              Thread.Sleep(100);
+              break;
+              case 'p':
+              _spotify.Pause();
+              break;
+              case 'b':
+              _spotify.Previous();
+              Thread.Sleep(100);
+              break;
+              case ' ':
+              if (_spotify.GetStatus().Playing) {
                 _spotify.Pause();
-                break;
-              case "b":
-                _spotify.Previous();
-                Thread.Sleep(100);
-                break;
-              case " ":
-                if(_spotify.GetStatus().Playing) {
-                  _spotify.Pause();
-                  PausedSong(_spotify.GetStatus().Track); 
-                }
-                else if (!(_spotify.GetStatus().Playing)){
-                  _spotify.Play();
-                  DisplaySong(_spotify.GetStatus().Track);               
-                }
-                break;
-              case "st":
+                PausedSong(_spotify.GetStatus().Track);
+              }
+              else if (!(_spotify.GetStatus().Playing)) {
+                _spotify.Play();
                 DisplaySong(_spotify.GetStatus().Track);
-                break;
-              case "a":
-                _spotify.Pause();
-                x = 1;
-                break;
+              }
+              break;
+              case 'z':
+              DisplaySong(_spotify.GetStatus().Track);
+              break;
+              case 'a':
+              _spotify.Pause();
+              x = 1;
+              break;
+              case 'm':
+              connectToMongo();
+              break;
             }
             #endregion
           }
         }
       }
-
       else {
         Console.WriteLine("ERROR: couldnt connect to spotify.");
       }
     }
 
     public static void PausedSong(Track song) {
-      Console.WriteLine("\nCurrently paused: {0} - {1} ({2})", song.TrackResource.Name, song.ArtistResource.Name, song.AlbumResource.Name);
+      Console.WriteLine("\n[{0}] : Currently paused: {1} - {2} ({3})", DateTime.Now.ToString(), song.TrackResource.Name, song.ArtistResource.Name, song.AlbumResource.Name);
     }
 
     public static void DisplaySong(Track song) {
-      Console.WriteLine("\nCurrently playing: {0} - {1} ({2})", song.TrackResource.Name, song.ArtistResource.Name, song.AlbumResource.Name);
+      Console.WriteLine("\n[{0}] : Currently playing: {1} - {2} ({3})", DateTime.Now.ToString(), song.TrackResource.Name, song.ArtistResource.Name, song.AlbumResource.Name);
+
     }
 
     private static void _spotify_OnPlayStateChange(object sender, PlayStateEventArgs e) {
@@ -91,10 +100,56 @@ namespace SPOTCMD {
       //playing/not playing toggle this will flag up..
     }
     private static void _spotify_OnTrackChange(object sender, TrackChangeEventArgs e) {
-      //Console.WriteLine("onTrackChange has been clled" );
+      //this writes the old track to the mongo database following the
+      //mongo_writeTrackToMongo(e.OldTrack);
+
       DisplaySong(e.NewTrack);
 
     }
+    public static void connectToMongo() {
+      var client = new MongoClient();
+      var db = client.GetDatabase("spotify");
+      var collection = db.GetCollection<Post>("Post");
+      //collection.DeleteMany("{ }");
+
+
+      //Create a Post to enter into the database.
+      var post = new Post() {
+        Title = "My First Post",
+        Body = "This isn't a very long post.",
+        CharCount = 27,
+        Comments = new List<Comment>
+        {
+            { new Comment() { TimePosted = new DateTime(2010,1,1),
+                              Email = "bob_mcbob@gmail.com",
+                              Body = "This article is too short!" } },
+            { new Comment() { TimePosted = new DateTime(2010,1,2),
+                              Email = "Jane.McJane@gmail.com",
+                              Body = "I agree with Bob." } }
+        }
+      };
+
+      //Save the post. This will perform an upsert. As in, if the post
+      //already exists, update it, otherwise insert it.
+      collection.InsertOne(post);
+
+      var document = collection.Find(new BsonDocument()).FirstOrDefault();
+      Console.WriteLine(document.Title);
+    }
+    public class Post {
+      public ObjectId Id { get; set; }
+      public string Title { get; set; }
+      public string Body { get; set; }
+      public int CharCount { get; set; }
+      public IList<Comment> Comments { get; set; }
+    }
+
+    public class Comment {
+      public DateTime TimePosted { get; set; }
+      public string Email { get; set; }
+      public string Body { get; set; }
+    }
+
     public static void WorkThreadFunction() {
       try {
         for (int k = 0; k <= 100; k++) {
@@ -105,6 +160,7 @@ namespace SPOTCMD {
           Console.SetCursorPosition(8, 4);
           Console.Write("{0}%", k);
           System.Threading.Thread.Sleep(50);
+          //Artist a  = new Artist();
         }
 
       }
